86b4a5d700978f56b50e1bc82650e3dcb2f48bcd
Chia-Yang Sun <triumph.sun@gmail.com>
2023-11-30 17:11:42 +0800

    [eslint] vim .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,29 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+/** @type {import('eslint').Linter.Config} */
+const config = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['@typescript-eslint'],
+    rules: {},
+};
+
+module.exports = config;
